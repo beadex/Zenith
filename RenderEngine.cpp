6bfa3a18edae0b230082ee52deca8ca3a0e0736a
Phong Tran <tranphongbb@outlook.com>
2026-03-29 15:59:39 +0700

    x64 build

--- RenderEngine.cpp
+++ RenderEngine.cpp
@@ -8,7 +8,7 @@ ZenithRenderEngine::ZenithRenderEngine(UINT width, UINT height, std::wstring nam
 	m_model(nullptr),
 	m_viewport(0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height)),
 	m_scissorRect(0, 0, static_cast<LONG>(width), static_cast<LONG>(height)),
-	m_cameraPos(XMVectorSet(2.2f, 1.6f, 7.0f, 1.0f)),
+	m_cameraPos(XMVectorSet(0.0f, 1.0f, 3.0f, 1.0f)),
 	m_cameraFront(XMVectorSet(0.0f, 0.0f, -1.0f, 0.0f)),
 	m_cameraUp(XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f)),
 	m_cbData{},
